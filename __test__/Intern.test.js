38f8611e2c42b481b3ab36e982996610239c2c8d
Jake Finn <lof.steven@gmail.com>
2022-01-19 19:19:30 -0600

    Add employee test functions.

---
+++ __test__/Intern.test.js
@@ -0,0 +1,22 @@
+const intern = require('./intern')
+
+// Need these properties and methods
+// name
+
+// id
+
+// email
+
+// getName()
+
+// getId()
+
+// getEmail()
+
+// getRole()—returns 'Employee'
+
+// school
+
+// getSchool()
+
+// getRole()—overridden to return 'Intern'
